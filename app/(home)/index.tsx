d40d2d328dacb611d775f82ebfb5c8f94c4bc79a
Ahbab Shahin Chowdhury <ahbabshahin7788@gmail.com>
2025-04-05 22:12:05 +0600

    Login with google added

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,24 +1,52 @@
 import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
-import { Link } from 'expo-router';
+import { Link, Stack, useRouter } from 'expo-router';
 import { Text, View } from 'react-native';
 import { SignOutButton } from '@/components/SignOutButton';
+import { useEffect, useState } from 'react';
 
 export default function Page() {
-	const { user } = useUser();
+	const { user, isLoaded } = useUser();
+		const router = useRouter();
+		const [hasMount, setHasMount] = useState(false)
+		// Redirect to sign-in when signed out
 
+		useEffect(() =>{
+			setHasMount(true)
+		}, [])
+		useEffect(() => {
+			if (hasMount && isLoaded && !user) {
+				router.replace('/(auth)/sign-in'); // Redirect without back button
+			}
+		}, [user, isLoaded, hasMount]);
 	return (
-		<View>
+		<View
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
 			<SignedIn>
-				<Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+				<Text>Hello</Text>
+				<Text>{user?.emailAddresses[0].emailAddress}</Text>
+				<Text>How May I help you?</Text>
 				<SignOutButton />
 			</SignedIn>
 			<SignedOut>
-				<Link href='/(auth)/sign-in'>
+				{/* <Link href='/(auth)/sign-in'>
 					<Text>Sign in</Text>
-				</Link>
-				<Link href='/(auth)/sign-up'>
+				</Link> */}
+				{/* <Link href='/(auth)/sign-up'>
 					<Text>Sign up</Text>
-				</Link>
+				</Link> */}
+				{/* <Stack>
+					<Stack.Screen
+						name='(auth)'
+						options={{
+							title: 'Sign in',
+						}}
+					/>
+				</Stack> */}
 			</SignedOut>
 		</View>
 	);
